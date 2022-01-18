5be06bd2ed5f4c1c03352504e2a3586a52342ff4
comong-god <hhshin@ygmail.net>
2022-01-18 15:36:22 +0900

    tnwjd

--- js/packages/web/src/views/home/components/SalesList/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -30,7 +30,7 @@ export const SalesListView = () => {
   return (
     <>
       <Banner
-        src="/main-banner.svg"
+        src="/ygplus/main-banner.svg"
         headingText="The amazing world of Metaplex."
         subHeadingText="Buy exclusive Metaplex NFTs."
         actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
